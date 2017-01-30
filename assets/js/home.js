60d4384c5c96e897cf26b05a288e7dacb01d07db
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2017-01-30 13:15:58 -0500

    equalize heights of vision elements

---
+++ assets/js/home.js
@@ -0,0 +1,25 @@
+(function () {
+  /* Equalize heights of the vision elements on the home page */
+  function equalizeVisionElements() {
+    var elements = document.getElementsByClassName("vision-element");
+    var tallest = 0;
+    for (var i = 0; i < elements.length; i++) {
+      if (elements[i].offsetHeight > tallest) {
+        elements[i].style.height = "auto";
+        tallest = elements[i].offsetHeight;
+      }
+    }
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.height = tallest + "px";
+    }
+  }
+
+  equalizeVisionElements();
+
+  /* Prevent the equalize function from continuously firing upon resize */
+  var resizeTimeout;
+  window.addEventListener("resize", function () {
+    window.clearTimeout(resizeTimeout);
+    resizeTimeout = window.setTimeout(equalizeVisionElements, 10);
+  });
+})();
