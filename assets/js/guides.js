910cd4a96bdfd36c4fc0b55cbe661fc02b70ae5a
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2017-03-09 18:53:52 -0500

    refactor for IE compatibility

--- assets/js/guides.js
+++ assets/js/guides.js
@@ -1,28 +1,18 @@
 $(document).ready(function () {
-  function getFilterValues(selector, checked) {
-    var modifier = checked ? ":checked" : ":not(:checked)";
-    var $selectedInputs = $(selector + modifier);
-    var matches = [];
-    $selectedInputs.each(function () {
-      matches.push($(this).val());
-    });
-    return matches;
-  }
-
   function hasIntersection(array1, array2) {
     var intersection = array1.filter(function (element) {
-      return array2.includes(element);
+      return array2.indexOf(element) !== -1;
     });
     return (intersection.length > 0);
   }
 
   function filtersUpdated() {
-    var selectedAudiences = getFilterValues(".guides-filter-audience", true);
-    var selectedCategories = getFilterValues(".guides-filter-category", true);
+    var selectedAudiences = $(".guides-filter-audience:checked").map(function () { return $(this).val(); }).get();
+    var selectedCategories = $(".guides-filter-category:checked").map(function () { return $(this).val(); }).get();
     sessionStorage.setItem("selectedAudiences", JSON.stringify(selectedAudiences));
     sessionStorage.setItem("selectedCategories", JSON.stringify(selectedCategories));
     $(".guides-table-row").each(function () {
-      var categorySelected = selectedCategories.includes($(this).data("category"));
+      var categorySelected = selectedCategories.indexOf($(this).data("category")) !== -1;
       var audienceSelected = hasIntersection(selectedAudiences, $(this).data("audiences").split(","));
       $(this).toggle(categorySelected && audienceSelected);
     });
@@ -36,13 +26,15 @@ $(document).ready(function () {
 
     if (selectedAudiences !== null) {
       $(".guides-filter-audience").each(function () {
-        $(this).prop("checked", selectedAudiences.includes($(this).val()));
+        var audienceSelected = selectedAudiences.indexOf($(this).val()) !== -1;
+        $(this).prop("checked", audienceSelected);
       });
     }
 
     if (selectedCategories !== null) {
       $(".guides-filter-category").each(function () {
-        $(this).prop("checked", selectedCategories.includes($(this).val()));
+        var categorySelected = selectedCategories.indexOf($(this).val()) !== -1;
+        $(this).prop("checked", categorySelected);
       });
     }
 
