dc2b0524b5f82c0e44360531d8d648bfe8629e63
Alicia Cyrus <aliciacyrus@users.noreply.github.com>
2017-12-29 11:18:41 -0500

    Create scrum_of_scrums.m

---
+++ _guides/scrum_of_scrums.m
@@ -0,0 +1,45 @@
+---
+title: Conducting a Scrum of Scrums
+category: Agile
+audiences:
+  - Developers
+  - Designers
+  - Project Managers
+---
+
+[Return to Agile Meetings]({{ site.baseurl }}/guides/Agile_Meetings_Goals_and_Benefits/#scrum-of-scrums)
+
+A **Scrum of Scrums**, or Meta-Scrum, may be used in environments that have multiple Scrum Teams. The purpose of the Scrum of Scrums is to provide a platform for cross-team collaboration and planning, highlighting dependencies, and addressing points of integration or conflict.
+
+The Scrum of Scrums is a time-boxed session in which a representative from each Team shares high-level updates on their respective team’s work and articulates their progress and impediments. Ideally, it should follow the various teams’ Daily Stand-ups, so that the latest information is communicated.
+
+While Daily Scrum meetings are used for quick updates, not for problem-solving, the Scrum of Scrums is not bound by the same rule. If a problem is identified in the Scrum of Scrums and all parties needed to make a decision are in attendance, the issue can be addressed at that time (just be sure scheduling permits).
+
+### Conducting a Meta-Scrum
+Team Representatives attending the Scrum of Scrums may be technical contributors, and / or the Team’s Scrum Master or Product Owner. Teams should also alternate their representatives over the course of the project. Often times the Scrum Master will attend with an alternating team member to ensure they are not only able to represent the Team, but are also better able to resolve issues that come up during the session.
+
+During the Scrum of Scrums, each Team Representative answers the following questions:
+* What has your Team done since we last met?
+* What will your Team do before we meet again?
+* Is anything slowing your Team down or getting in their way?
+* Are you about to put something in another Team’s way?
+
+While it may be suggested to have the session on a daily basis, the frequency of the Meta-Scrum should be determined by the Teams and scheduled as it will add the most value (i.e. daily, 2x per week, etc.).
+
+### Benefits of a Meta-Scrum
+The Scrum of Scrums is also an approach to scale Scrum up to larger groups (typically over a 12+ people), where the groups will be divided into Agile Teams of 5 - 10 members. While the various individual Teams are able to focus on their work, the Meta-Scrum also ensures cross-coordination and communication.
+
+### Cross-Team Standups
+Cross-Teams Stand-ups are also used in scaled Agile approaches. While often confused with Scrum of Scrums (Meta-Scrums), Cross-Team Stand-ups differ in that instead of one representative attending the cross-functional stand-up from each Team, all team members from each Team participate.
+
+### Good Reads
+These are good references for conducting a Scrum of Scrums:
+* [5 Must-Have Agenda Items in a Scrum of Scrum Meeting](https://www.scrumalliance.org/community/articles/2016/march/5-must-have-agenda-items-in-a-scrum-of-scrum-meeti)
+* [Advice on Conducting the Scrum of Scrums Meeting](https://www.mountaingoatsoftware.com/articles/advice-on-conducting-the-scrum-of-scrums-meeting)
+* [AgileAlliance: Scrum of Scrums](https://www.agilealliance.org/glossary/scrum-of-scrums/)
+* [Daily Standup and Scrum of Scrums - what should be the order of meetings?](https://www.scrum.org/forum/scrum-forum/5723/daily-standup-and-scrum-scrums-what-should-be-order-meetings)
+* [Making A Cross-Team Standup Work](https://medium.com/@devemail17/making-a-cross-team-standup-work-69237ebf8789)
+* [Scrum, Inc: Scrum of Scrums](https://www.scruminc.com/scrum-of-scrums/)
+* [Scrum in practice: the Scrum of Scrums meeting](https://manifesto.co.uk/scrum-of-scrums-meeting/)
+* [Using Scrum of Scrums with Agile Teams to Coordinate and Collaborate](https://www.infoq.com/news/2014/03/scrum-of-scrums)
+* [What is a Scrum of Scrums?](http://www.velocitypartners.net/blog/2017/01/17/what-is-a-scrum-of-scrums/)
