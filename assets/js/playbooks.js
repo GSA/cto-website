18f64b3a63295acdde19d07165177fbdc0c0ef22
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2018-09-20 15:49:08 -0400

    add nav scrolling

---
+++ assets/js/playbooks.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  const topOffset = -0.85 * window.innerHeight
+
+  $(":header").each(function (index, header) {
+    const sectionWatcher = scrollMonitor.create(header, { top: topOffset })
+    sectionWatcher.enterViewport(function () {
+      $(".usa-sidenav-list a").removeClass("usa-current")
+      $(`.usa-sidenav-list a[href='#${sectionWatcher.watchItem.id}']`).addClass("usa-current")
+    })
+  })
+
+  $(".usa-sidenav-list a").click(function (event) {
+    const target = $(this).attr("href")
+    $("html, body").animate({
+      scrollTop: $(target).offset().top - $("#nav-primary").height() - 20
+    }, 400, null, function () {
+      $(".usa-sidenav-list a").removeClass("usa-current")
+      $(`.usa-sidenav-list a[href='${target}']`).addClass("usa-current")
+    })
+  })
+})
