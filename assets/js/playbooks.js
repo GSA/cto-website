8f013171088baae1fa62e359c5d0486ae27750f1
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2018-09-20 15:55:36 -0400

    downgrade to ES5 syntax

--- assets/js/playbooks.js
+++ assets/js/playbooks.js
@@ -1,21 +1,23 @@
 $(document).ready(function () {
-  const topOffset = -0.85 * window.innerHeight
+  var topOffset = -0.85 * window.innerHeight
 
   $(":header").each(function (index, header) {
-    const sectionWatcher = scrollMonitor.create(header, { top: topOffset })
+    var sectionWatcher = scrollMonitor.create(header, { top: topOffset })
     sectionWatcher.enterViewport(function () {
       $(".usa-sidenav-list a").removeClass("usa-current")
-      $(`.usa-sidenav-list a[href='#${sectionWatcher.watchItem.id}']`).addClass("usa-current")
+      $(".usa-sidenav-list a[href='#" + sectionWatcher.watchItem.id + "']").addClass("usa-current")
     })
   })
 
   $(".usa-sidenav-list a").click(function (event) {
-    const target = $(this).attr("href")
+    var target = $(this).attr("href")
     $("html, body").animate({
       scrollTop: $(target).offset().top - $("#nav-primary").height() - 20
     }, 400, null, function () {
       $(".usa-sidenav-list a").removeClass("usa-current")
-      $(`.usa-sidenav-list a[href='${target}']`).addClass("usa-current")
+      $(".usa-sidenav-list a[href='#" + target + "']").addClass("usa-current")
     })
   })
+
+  // dynamically add back to top links?
 })
