af0adb42f697a0e34cbed7c420dcba8807daacc0
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2022-03-03 16:41:44 -0700

    implement a 'Return to top' button on all pages

--- assets/js/global.js
+++ assets/js/global.js
@@ -5,10 +5,35 @@ Mousetrap.bind('/', function () {
 });
 
 (function () {
+  //
   // Add figure/figcaption elements to images that have a title attribute, using the title as the caption.
+  //
+
   $("img[title]").each(function () {
     $(this)
       .wrap("<figure>")
       .after(`<figcaption>${$(this).attr("title")}</figcaption>`);
   });
+
+
+  //
+  // Show a "Return to the top of this page" button after the user scrolls down
+  //
+
+  // How far the user scrolls before the button appears
+  const topLinkThreshold = 300;
+
+  $(window).on('scroll', function () {
+    if (window.scrollY > topLinkThreshold) {
+      $(".top-link").fadeIn();
+    } else {
+      $(".top-link").fadeOut();
+    }
+  });
+
+  $(".top-link").on('click', function () {
+    $("html").animate({
+      scrollTop: 0
+    }, 200);
+  })
 })();
